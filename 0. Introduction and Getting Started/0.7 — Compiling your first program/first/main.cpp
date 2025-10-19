950467bfa7f743587cdeb15655b0a1b9c0c9bd03
Kaan <kaanozarslan@ogr.eskisehir.edu.tr>
2025-10-19 11:31:21 +0300

    0.7

---
+++ 0. Introduction and Getting Started/0.7 — Compiling your first program/first/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    std::cout << "My demons!" << std::endl;
+    auto input = std::cin.get();
+
+    std::cout << "Your input is: " << input << std::endl;
+
+    return 0;
+
+}
